dc4146ae0c153be79ed1685dfc4dc16719b9e9c6
Oleksandr Shvets <pascal36@yandex.com>
2017-05-30 11:14:01 +0200

    weapon test

---
+++ ShootingGame/WeaponTest/UnitTestWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShootingGame;
+using Microsoft.Xna.Framework;
+
+namespace WeaponTest
+{
+    [TestClass]
+    public class UnitTestWeapon
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Weapon w = new Weapon("GUN", 10, 30, 1000, WeaponType.BoltAction);
+            w.TotalAmmo = 99;
+            Assert.AreEqual(99, w.TotalAmmo);
+        }
+    }
+}
